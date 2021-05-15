280cf30622a8cc214d1c7440a83c81a214c23fbb
vinay-kodam <64982203+vinay-kodam@users.noreply.github.com>
2021-05-15 22:45:10 +0200

    Create Basic_speech_recognition_code.py

---
+++ NLP/Scripts/Basic_speech_recognition_code.py
@@ -0,0 +1,20 @@
+************https://www.tutorialspoint.com/artificial_intelligence_with_python/artificial_intelligence_with_python_speech_recognition.htm
+We need to install the following packages for this −
+Pyaudio − It can be installed by using pip install Pyaudio command.
+SpeechRecognition − This package can be installed by using pip install SpeechRecognition.
+Google-Speech-API − It can be installed by using the command pip install google-api-python-client.***********
+
+
+#Code
+import speech_recognition as sr
+recording = sr.Recognizer()
+
+with sr.Microphone() as source: 
+    recording.adjust_for_ambient_noise(source)
+    print("Please Say something:")
+    audio = recording.listen(source)
+   
+try:
+   print("You said: \n" + recording.recognize_google(audio))
+except Exception as e:
+   print(e)
