2dcb34107875041821c3b3d70139ac93ef70fa71
vinay-kodam <64982203+vinay-kodam@users.noreply.github.com>
2021-05-15 22:48:23 +0200

    Pyaudio package installation solution added

--- NLP/Scripts/Basic_speech_recognition_code.py
+++ NLP/Scripts/Basic_speech_recognition_code.py
@@ -1,6 +1,6 @@
 ************https://www.tutorialspoint.com/artificial_intelligence_with_python/artificial_intelligence_with_python_speech_recognition.htm
 We need to install the following packages for this −
-Pyaudio − It can be installed by using pip install Pyaudio command.
+Pyaudio − It can be installed by using pip install Pyaudio command. (should download Pyaudio.whl file to install Pyaudio package if python versions>3.6)
 SpeechRecognition − This package can be installed by using pip install SpeechRecognition.
 Google-Speech-API − It can be installed by using the command pip install google-api-python-client.***********
 
